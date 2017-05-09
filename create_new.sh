d16bad0bbcc3d0a03a52d079af8ac5ab2b264866
hernad <hernad@bring.out.ba>
2017-05-09 12:09:27 +0200

    ams1

--- create_new.sh
+++ create_new.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-scw run --name=greenbox-scw-0 --commercial-type=VC1S  Alpine_Linux
+REGION=ams1  # ams1, par1
+SERVER_NAME=greenbox-scw-0
+SERVER_TYPE=VC1S
+
+scw run --region=$REGION --name=$SERVER_NAME --commercial-type=$SERVER_TYPE  Alpine_Linux
  
-#sleep 10
-#./scw-ipxe-create.expect \
-#     "initrd http://dl.bintray.com/hernad/greenbox/greenbox-4.3.1.iso" \
-#     "chain http://boot.salstar.sk/memdisk iso raw"
 
